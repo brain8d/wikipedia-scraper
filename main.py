038e4a47ec958b4022b65fe6c6e87a5761e7ed36
bdaza <brian.daza@proton.me>
2024-02-02 16:24:08 +0100

    checking api and cookie status features

--- main.py
+++ main.py
@@ -1,40 +1,56 @@
 # testing here
 from src.scraper import WikipediaScraper
-import json
+import json, requests
 
-url = "https://country-leaders.onrender.com"
-country_endpoint = "/countries"
-leaders_endpoint = "/leaders"
-cookies_endpoint = "/cookie"
+if __name__ == "__main__":
+    url = "https://country-leaders.onrender.com"
+    country_endpoint = "/countries"
+    leaders_endpoint = "/leaders"
+    cookies_endpoint = "/cookie"
+    status_endpoint = "/status"
 
-scrap = WikipediaScraper(
-    base_url= url,
-    country_endpoint=country_endpoint,
-    leaders_endpoint=leaders_endpoint,
-    cookies_endpoint=cookies_endpoint)
+    scrap = WikipediaScraper(
+        base_url= url,
+        country_endpoint= country_endpoint,
+        leaders_endpoint= leaders_endpoint,
+        cookies_endpoint= cookies_endpoint,
+        status_endpoint = status_endpoint)
 
+    # Check status API before starting
+    # If output "Alive"
+    print("Checking API status...")
+    print("Status API: ", scrap.check_status())
+    print("Continuing...")
+    if scrap.check_status():
+        print("Checking cookies...")
+        if scrap.check_cookie().status_code == 200:
+            print(scrap.check_cookie().text)
+            countries = scrap.get_countries().json()
 
-countries = scrap.get_countries().json()
-all_leaders= {}
-for country in countries:
-    # Creates a dictionary per country with the leaders' data and stores it back in the leaders_data
-    scrap.get_leaders(country)
-    all_leaders[country] = scrap.leaders_data
-    scrap.leaders_data = all_leaders
+            all_leaders= {}
+            for country in countries:
+                # Creates a dictionary per country with the leaders' data and stores it back in the leaders_data
+                scrap.get_leaders(country)
+                all_leaders[country] = scrap.leaders_data
+                scrap.leaders_data = all_leaders
 
-'''
-Now there is a dictionary per country that contains the original leader data:
-A list of dictionaries with all the leader features
-The next step is to access the country dictionary, access the value, which is the original list of leader data
-and retrieve the wiki URL
-'''
+            '''
+            Now there is a dictionary per country that contains the original leader data:
+            A list of dictionaries with all the leader features
+            The next step is to access the country dictionary, access the value, which is the original list of leader data
+            and retrieve the wiki URL
+            '''
 
-# Iterate over the countries in dictionary
-for country in scrap.leaders_data.values():
-    # Access the value of each country -> a list of leaders
-    for leader in country:
-        link = leader.get("wikipedia_url")
-        leader["wikipedia_info"] = scrap.get_first_paragraph(link) 
+            # Iterate over the countries in dictionary
+            for country in scrap.leaders_data.values():
+                # Access the value of each country -> a list of leaders
+                for leader in country:
+                    link = leader.get("wikipedia_url")
+                    leader["wikipedia_info"] = scrap.get_first_paragraph(link) 
 
-# Save to file..
-scrap.to_json_file("leaders_data.json")
+            # Save to file..
+            scrap.to_json_file("leaders_data.json")
+
+        else:
+            # if cookies invalid return cookie check message
+            print(scrap.check_cookie().text)
--- src/scraper.py
+++ src/scraper.py
@@ -1,20 +1,6 @@
 # Script for wikipedia scraper
-import requests
-import json
-import re
+import requests, json, re
 from bs4 import BeautifulSoup
-
-
-
-root_url = "https://country-leaders.onrender.com"
-status_url = "https://country-leaders.onrender.com/status"
-r = requests.get(status_url)
-
-# check the status_code 
-if r.status_code == 200:
-    print(r.text)
-else:
-    print(r.status_code)
 
 class WikipediaScraper:
     '''
@@ -26,33 +12,43 @@ class WikipediaScraper:
     cookie: object is the cookie object used for the API calls
     ):
     '''
-    def __init__(self, base_url: str, country_endpoint: str, leaders_endpoint: str, cookies_endpoint: str, cookie = None):
+    def __init__(self, base_url: str, country_endpoint: str, leaders_endpoint: str, cookies_endpoint: str, status_endpoint: str, cookie = None):
         self.base_url = base_url
         self.country_endpoint = country_endpoint
         self.leaders_endpoint = leaders_endpoint
         self.cookies_endpoint = cookies_endpoint
         self.leaders_data = ""
-       # self.paragraph = None
+        self.status_endpoint = status_endpoint
 
-    # incorporate here a status check when initializing the object with
-    # the get status 
+    def check_status(self):
+        status = requests.get(self.base_url+self.status_endpoint)
+        if status.status_code == 200:
+            return status.text
+        else:
+            return status.status_code
 
     def get_cookie(self):
         ''' gets cookie for the API '''
         cookie = requests.get(self.base_url+self.cookies_endpoint)
         return cookie.cookies
 
+    def check_cookie(self):
+        ''' checks cookie status'''
+        cookie_check = requests.get(self.base_url+"/check", cookies=self.get_cookie())
+        return cookie_check
 
     def get_countries(self):
         ''' gets a list of countries supported by the API '''
         countries = requests.get(self.base_url+self.country_endpoint, cookies=self.get_cookie())
         return countries
 
+
     def get_leaders(self,  country: str):
         '''get leaders of specified country, does not return anything but populates self.leaders_data '''
         leaders = {'country': country}
         leader = requests.get(self.base_url+self.leaders_endpoint, cookies=self.get_cookie(), params=leaders)
         self.leaders_data = json.loads(leader.text)
+
 
     def get_first_paragraph(self, wikipedia_url: str) -> str:
         ''' gets the first paragraph on leaders' wikipedia URL, uses the BOLD letters to identify first paragraph '''
